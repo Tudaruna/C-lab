5693525c3b6f37817911485ce6d966422e366caf
feroswin <vitalkaivano009@mail.ru>
2023-03-19 00:15:46 +0300

    load lab 6

---
+++ lab6/textEditor/textEditor/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace textEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFormat = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFill = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAlignment = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiMarker = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiRedo = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.tsmiToLeft = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiToCenter = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiToRight = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.tsmiFormat,
+            this.tsmiEdit,
+            this.tsmiHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNew,
+            this.tsmiOpen,
+            this.tsmiSave,
+            this.tsmiExit});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(48, 20);
+            this.tsmiFile.Text = "Файл";
+            // 
+            // tsmiNew
+            // 
+            this.tsmiNew.Name = "tsmiNew";
+            this.tsmiNew.Size = new System.Drawing.Size(180, 22);
+            this.tsmiNew.Text = "Создать";
+            this.tsmiNew.Click += new System.EventHandler(this.tsmiNew_Click);
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.Size = new System.Drawing.Size(180, 22);
+            this.tsmiOpen.Text = "Открыть";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // tsmiSave
+            // 
+            this.tsmiSave.Name = "tsmiSave";
+            this.tsmiSave.Size = new System.Drawing.Size(180, 22);
+            this.tsmiSave.Text = "Сохранить";
+            this.tsmiSave.Click += new System.EventHandler(this.tsmiSave_Click);
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(180, 22);
+            this.tsmiExit.Text = "Выход";
+            // 
+            // tsmiFormat
+            // 
+            this.tsmiFormat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFont,
+            this.tsmiFill,
+            this.tsmiAlignment,
+            this.tsmiMarker});
+            this.tsmiFormat.Name = "tsmiFormat";
+            this.tsmiFormat.Size = new System.Drawing.Size(62, 20);
+            this.tsmiFormat.Text = "Формат";
+            // 
+            // tsmiEdit
+            // 
+            this.tsmiEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiUndo,
+            this.tsmiRedo,
+            this.tsmiCopy,
+            this.tsmiCut,
+            this.tsmiPaste,
+            this.tsmiDelete});
+            this.tsmiEdit.Name = "tsmiEdit";
+            this.tsmiEdit.Size = new System.Drawing.Size(108, 20);
+            this.tsmiEdit.Text = "Редактирование";
+            this.tsmiEdit.Click += new System.EventHandler(this.tsmiEdit_Click);
+            // 
+            // tsmiHelp
+            // 
+            this.tsmiHelp.Name = "tsmiHelp";
+            this.tsmiHelp.Size = new System.Drawing.Size(65, 20);
+            this.tsmiHelp.Text = "Справка";
+            // 
+            // tsmiFont
+            // 
+            this.tsmiFont.Name = "tsmiFont";
+            this.tsmiFont.Size = new System.Drawing.Size(180, 22);
+            this.tsmiFont.Text = "Шрифт";
+            this.tsmiFont.Click += new System.EventHandler(this.tsmiFont_Click);
+            // 
+            // tsmiFill
+            // 
+            this.tsmiFill.Name = "tsmiFill";
+            this.tsmiFill.Size = new System.Drawing.Size(180, 22);
+            this.tsmiFill.Text = "Заливка";
+            this.tsmiFill.Click += new System.EventHandler(this.tsmiFill_Click);
+            // 
+            // tsmiAlignment
+            // 
+            this.tsmiAlignment.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiToLeft,
+            this.tsmiToCenter,
+            this.tsmiToRight});
+            this.tsmiAlignment.Name = "tsmiAlignment";
+            this.tsmiAlignment.Size = new System.Drawing.Size(180, 22);
+            this.tsmiAlignment.Text = "Выравнивание";
+            // 
+            // tsmiMarker
+            // 
+            this.tsmiMarker.Name = "tsmiMarker";
+            this.tsmiMarker.Size = new System.Drawing.Size(180, 22);
+            this.tsmiMarker.Text = "Маркер";
+            this.tsmiMarker.Click += new System.EventHandler(this.tsmiMarker_Click);
+            // 
+            // tsmiUndo
+            // 
+            this.tsmiUndo.Name = "tsmiUndo";
+            this.tsmiUndo.Size = new System.Drawing.Size(185, 22);
+            this.tsmiUndo.Text = "Отменить действие";
+            // 
+            // tsmiRedo
+            // 
+            this.tsmiRedo.Name = "tsmiRedo";
+            this.tsmiRedo.Size = new System.Drawing.Size(185, 22);
+            this.tsmiRedo.Text = "Повторить действие";
+            // 
+            // tsmiCopy
+            // 
+            this.tsmiCopy.Name = "tsmiCopy";
+            this.tsmiCopy.Size = new System.Drawing.Size(185, 22);
+            this.tsmiCopy.Text = "Копировать";
+            this.tsmiCopy.Click += new System.EventHandler(this.tsmiCopy_Click);
+            // 
+            // tsmiCut
+            // 
+            this.tsmiCut.Name = "tsmiCut";
+            this.tsmiCut.Size = new System.Drawing.Size(185, 22);
+            this.tsmiCut.Text = "Вырезать";
+            this.tsmiCut.Click += new System.EventHandler(this.tsmiCut_Click);
+            // 
+            // tsmiPaste
+            // 
+            this.tsmiPaste.Name = "tsmiPaste";
+            this.tsmiPaste.Size = new System.Drawing.Size(185, 22);
+            this.tsmiPaste.Text = "Вставить";
+            this.tsmiPaste.Click += new System.EventHandler(this.tsmiPaste_Click);
+            // 
+            // tsmiDelete
+            // 
+            this.tsmiDelete.Name = "tsmiDelete";
+            this.tsmiDelete.Size = new System.Drawing.Size(185, 22);
+            this.tsmiDelete.Text = "Удалить";
+            this.tsmiDelete.Click += new System.EventHandler(this.tsmiDelete_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 404);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Простой текст|*.txt|RTF-текст|*.rtf|Все файлы|*.*";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Простой текст|*.txt|RTF-текст|*.rtf|Все файлы|*.*";
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.ShowColor = true;
+            // 
+            // tsmiToLeft
+            // 
+            this.tsmiToLeft.Name = "tsmiToLeft";
+            this.tsmiToLeft.Size = new System.Drawing.Size(180, 22);
+            this.tsmiToLeft.Text = "По левому краю";
+            this.tsmiToLeft.Click += new System.EventHandler(this.tsmiToLeft_Click);
+            // 
+            // tsmiToCenter
+            // 
+            this.tsmiToCenter.Name = "tsmiToCenter";
+            this.tsmiToCenter.Size = new System.Drawing.Size(180, 22);
+            this.tsmiToCenter.Text = "По центру";
+            this.tsmiToCenter.Click += new System.EventHandler(this.tsmiToCenter_Click);
+            // 
+            // tsmiToRight
+            // 
+            this.tsmiToRight.Name = "tsmiToRight";
+            this.tsmiToRight.Size = new System.Drawing.Size(180, 22);
+            this.tsmiToRight.Text = "По правому краю";
+            this.tsmiToRight.Click += new System.EventHandler(this.tsmiToRight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNew;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSave;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFormat;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFont;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFill;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAlignment;
+        private System.Windows.Forms.ToolStripMenuItem tsmiMarker;
+        private System.Windows.Forms.ToolStripMenuItem tsmiEdit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiUndo;
+        private System.Windows.Forms.ToolStripMenuItem tsmiRedo;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopy;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCut;
+        private System.Windows.Forms.ToolStripMenuItem tsmiPaste;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDelete;
+        private System.Windows.Forms.ToolStripMenuItem tsmiHelp;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiToLeft;
+        private System.Windows.Forms.ToolStripMenuItem tsmiToCenter;
+        private System.Windows.Forms.ToolStripMenuItem tsmiToRight;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ lab6/textEditor/textEditor/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace textEditor
+{
+    public partial class Form1 : Form
+    {
+        private bool changed = false;
+        private string fileName = null;
+        private string textCopy = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Новый документ - BlokNote";
+            statusStrip1.Items[0].Text = "Не сохранено";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            changed = true;
+            statusStrip1.Items[0].Text = "Не сохранено";
+        }
+
+        private void saveFileAs()
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK && saveFileDialog1.FileName != null)
+            {
+                fileName = saveFileDialog1.FileName;
+                saveFile();
+            }
+        }
+
+        private void saveFile()
+        {
+            try
+            {
+                richTextBox1.SaveFile(fileName, RichTextBoxStreamType.RichText);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось сохранить файл! Информация об ошибке" +
+                    ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tsmiSave_Click(object sender, EventArgs e)
+        {
+            if (fileName == null)
+                saveFileAs();
+            else
+                saveFile();
+            this.Text = Path.GetFileName(fileName) + " - BlokNot";
+            statusStrip1.Items[0].Text = "Сохранено";
+            changed = false;
+        }
+
+        private bool closeFileQuery()
+        {
+            if (changed)
+            {
+                DialogResult result = MessageBox.Show("Сохранить изменения?",
+                                                      "Предупреждение",
+                                                      MessageBoxButtons.YesNoCancel,
+                                                      MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    if (fileName == null)
+                        saveFileAs();
+                    else
+                        saveFile();
+                }
+                else
+                {
+                    if (result == DialogResult.Cancel)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void openFile()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileName != null) 
+            { 
+                try
+                {
+                    richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                }
+                catch (FormatException fe)
+                {
+                    richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                }
+                changed = false;
+                fileName = openFileDialog1.FileName;
+            }
+        }
+
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            if (closeFileQuery())
+            {
+                openFile();
+                this.Text = Path.GetFileName(fileName) + " - BlokNot";
+                statusStrip1.Items[0].Text = "Сохранено";
+
+            }
+        }
+
+        private void newFile ()
+        {
+            richTextBox1.Clear();
+            changed = false;
+            fileName = null;
+        }
+
+        private void tsmiNew_Click(object sender, EventArgs e)
+        {
+            if (closeFileQuery())
+            {
+                newFile();
+                this.Text = "Новый документ BlokNot";
+                statusStrip1.Items[0].Text = "Не сохранено";
+            }
+        }
+
+        private void tsmiFont_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+                richTextBox1.SelectionColor = fontDialog1.Color;
+            } 
+        }
+
+        private void tsmiToLeft_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void tsmiToCenter_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void tsmiToRight_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void tsmiMarker_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionBullet = !richTextBox1.SelectionBullet;
+        }
+
+        private void tsmiEdit_Click(object sender, EventArgs e)
+        {
+            tsmiUndo.Enabled = richTextBox1.CanUndo;
+            tsmiRedo.Enabled = richTextBox1.CanRedo;
+        }
+
+        private void tsmiDelete_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectedText = "";
+        }
+
+        private void tsmiFill_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            richTextBox1.SelectionBackColor = colorDialog1.Color;
+        }
+
+        private void tsmiCopy_Click(object sender, EventArgs e)
+        {
+            textCopy = richTextBox1.SelectedText;
+        }
+
+        private void tsmiCut_Click(object sender, EventArgs e)
+        {
+            textCopy = richTextBox1.SelectedText;
+            richTextBox1.SelectedText = "";
+        }
+
+        private void tsmiPaste_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectedText = textCopy;
+        }
+    }
+}
