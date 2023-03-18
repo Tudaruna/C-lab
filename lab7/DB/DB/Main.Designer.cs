175c0b10c9f8ebc291e4c8b92f28a2b65c3fb20f
feroswin <vitalkaivano009@mail.ru>
2023-03-19 01:50:48 +0300

    load lab 7

---
+++ lab7/DB/DB/Main.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace DB
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.id_txt = new System.Windows.Forms.TextBox();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.ad_txt = new System.Windows.Forms.TextBox();
+            this.tel_txt = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.update_btn = new System.Windows.Forms.Button();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.clear_btn);
+            this.tabPage1.Controls.Add(this.delete_btn);
+            this.tabPage1.Controls.Add(this.update_btn);
+            this.tabPage1.Controls.Add(this.save_btn);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.tel_txt);
+            this.tabPage1.Controls.Add(this.ad_txt);
+            this.tabPage1.Controls.Add(this.name_txt);
+            this.tabPage1.Controls.Add(this.id_txt);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Entry";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Report";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(294, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone";
+            // 
+            // id_txt
+            // 
+            this.id_txt.Location = new System.Drawing.Point(105, 46);
+            this.id_txt.Name = "id_txt";
+            this.id_txt.Size = new System.Drawing.Size(145, 20);
+            this.id_txt.TabIndex = 4;
+            // 
+            // name_txt
+            // 
+            this.name_txt.Location = new System.Drawing.Point(105, 92);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(145, 20);
+            this.name_txt.TabIndex = 5;
+            // 
+            // ad_txt
+            // 
+            this.ad_txt.Location = new System.Drawing.Point(105, 138);
+            this.ad_txt.Multiline = true;
+            this.ad_txt.Name = "ad_txt";
+            this.ad_txt.Size = new System.Drawing.Size(145, 75);
+            this.ad_txt.TabIndex = 6;
+            // 
+            // tel_txt
+            // 
+            this.tel_txt.Location = new System.Drawing.Point(349, 46);
+            this.tel_txt.Name = "tel_txt";
+            this.tel_txt.Size = new System.Drawing.Size(100, 20);
+            this.tel_txt.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 263);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(399, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(325, 88);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(124, 23);
+            this.save_btn.TabIndex = 9;
+            this.save_btn.Text = "Save";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // update_btn
+            // 
+            this.update_btn.Location = new System.Drawing.Point(325, 133);
+            this.update_btn.Name = "update_btn";
+            this.update_btn.Size = new System.Drawing.Size(124, 23);
+            this.update_btn.TabIndex = 10;
+            this.update_btn.Text = "Update";
+            this.update_btn.UseVisualStyleBackColor = true;
+            this.update_btn.Click += new System.EventHandler(this.update_btn_Click);
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Location = new System.Drawing.Point(325, 180);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(124, 23);
+            this.delete_btn.TabIndex = 11;
+            this.delete_btn.Text = "Delete";
+            this.delete_btn.UseVisualStyleBackColor = true;
+            this.delete_btn.Click += new System.EventHandler(this.delete_btn_Click);
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Location = new System.Drawing.Point(325, 221);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(124, 23);
+            this.clear_btn.TabIndex = 12;
+            this.clear_btn.Text = "Clear";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Main";
+            this.Text = "Работа с БД";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clear_btn;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.Button update_btn;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tel_txt;
+        private System.Windows.Forms.TextBox ad_txt;
+        private System.Windows.Forms.TextBox name_txt;
+        private System.Windows.Forms.TextBox id_txt;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab7/DB/DB/Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DB
+{
+    public partial class Main : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=Examen;Trusted_Connection=true");
+        
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+            load_data();
+        }
+
+        private void load_data()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT*FROM [User]", connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            dataGridView1.DataSource = data;
+        }
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("INSERT INTO [User](Id, Name, Address, Phone) " +
+                $"VALUES ('{id_txt.Text}', '{name_txt.Text}', '{ad_txt.Text}', '{tel_txt.Text}')", connection);
+            command.ExecuteNonQuery();
+            load_data();
+            clear_fun();
+            MessageBox.Show("saved");
+        }
+
+        private void clear_fun()
+        {
+            id_txt.Text = "";
+            name_txt.Text = "";
+            ad_txt.Text = "";
+            tel_txt.Text = "";
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            clear_fun();
+        }
+
+        private void update_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand($"UPDATE [User] SET Name='{name_txt.Text}', Address='{ad_txt.Text}', Phone='{tel_txt.Text}'" +
+                $"WHERE Id='{id_txt.Text}'", connection);
+            command.ExecuteNonQuery();
+            load_data();
+            clear_fun();
+            MessageBox.Show("Updated");
+        }
+
+        private void delete_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand($"DELETE FROM [User] WHERE Id='{id_txt.Text}'", connection);
+            command.ExecuteNonQuery();
+            load_data();
+            clear_fun();
+            MessageBox.Show("Deleted");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int Num = dataGridView1.CurrentCell.RowIndex;
+            id_txt.Text = dataGridView1.Rows[Num].Cells[0].Value.ToString();
+            name_txt.Text = dataGridView1.Rows[Num].Cells[1].Value.ToString();
+            ad_txt.Text = dataGridView1.Rows[Num].Cells[2].Value.ToString();
+            tel_txt.Text = dataGridView1.Rows[Num].Cells[3].Value.ToString();
+        }
+    }
+}
